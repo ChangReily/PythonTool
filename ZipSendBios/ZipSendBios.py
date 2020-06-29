6a381f2a08b4b569aac1bf05397e9b12bd1071dc
Chang, Reily <reily.chang@hp.com>
2020-06-29 14:24:16 +0800

    Get Bios ID from environment variable

--- ZipSendBios/ZipSendBios.py
+++ ZipSendBios/ZipSendBios.py
@@ -150,13 +150,13 @@ def UnzipFile(FilePath, ExtractPath):
     print ('--------------------\n')
     return
 
-def GetBiosBinPath(TempFolder):
+def GetBiosBinPath(TempFolder, ProjectId):
     Found=False
     TargetBiosPath=''
     CmdStr=f'dir {TempFolder} /b'
     Buffer=CallSubprocess(CmdStr).StdoutBuffer()
     for idx in range(0, len(Buffer), 1):
-        match = re.search('T95_[0-9A-Fa-f]{6}_[0-9A-Fa-f]{2}.bin', Buffer[idx])
+        match = re.search(ProjectId+'_[0-9A-Fa-f]{6}_[0-9A-Fa-f]{2}.bin', Buffer[idx])
         if match:
             # print (Buffer[idx])
             BiosName=Buffer[idx].strip()
@@ -181,6 +181,7 @@ if __name__ == "__main__":
     BiosStorePath=os.getenv('BiosStorePath')
     Workspace=os.getenv('WORKSPACE')
     BuildNumber=os.getenv('BUILD_NUMBER')
+    ProjectId=os.getenv('PROJECT_ID')
 
     TargetFolder=os.path.join(Workspace, f'Build_{BuildNumber}')
     if os.path.isdir(TargetFolder) == False:
@@ -191,14 +192,14 @@ if __name__ == "__main__":
     ReleaseBioaPackage=os.path.join(BiosStorePath,'Fv_Release.7z')
     UnzipFile(ReleaseBioaPackage, Workspace)
     TempFolder=os.path.join(Workspace, os.path.splitext(os.path.basename(ReleaseBioaPackage))[0])
-    BiosBinPath=GetBiosBinPath(TempFolder)
+    BiosBinPath=GetBiosBinPath(TempFolder, ProjectId)
     # ZipReleaseBiosFile=ZipBiosBin(BiosBinPath)
     shutil.copy2(BiosBinPath, os.path.join(TargetFolder, 'Release'))
 
     DebugBioaPackage=os.path.join(BiosStorePath,'Fv_Debug.7z')
     UnzipFile(DebugBioaPackage, Workspace)
     TempFolder=os.path.join(Workspace, os.path.splitext(os.path.basename(DebugBioaPackage))[0])
-    BiosBinPath=GetBiosBinPath(TempFolder)
+    BiosBinPath=GetBiosBinPath(TempFolder, ProjectId)
     shutil.copy2(BiosBinPath, os.path.join(TargetFolder, 'Debug'))
     # ZipBiosBin(BiosBinPath)
 
